ba8e8adb7dd257e15130a9eefba938956f6de22c
Jeivardan <jeivardanvenkatesh@gmail.com>
2019-08-18 10:20:31 +0530

    Leaderboard and proxy
    basic leaderboard page is up and proxy for the api calls is updated.

---
+++ src/views/Pages/Home/Leaderboard/tablerow.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class TableRow extends Component {
+  render() {
+    return (
+        <tr>
+          <td>
+            {this.props.obj.leader_id}
+          </td>
+          <td>
+            {this.props.obj.leader_name}
+          </td>
+          <td>
+            {this.props.obj.leader_points}
+          </td>
+        </tr>
+    );
+  }
+}
+
+export default TableRow;
