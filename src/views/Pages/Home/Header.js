15a7e58ad159a5f1292370bd921fb2c4275fe38d
Aakash Hemadri <aakashhemadri123@gmail.com>
2019-08-21 19:01:16 +0530

    Fixed Header

--- src/views/Pages/Home/Header.js
+++ src/views/Pages/Home/Header.js
@@ -4,10 +4,12 @@ import { Jumbotron } from 'react-bootstrap';
 export default class Header extends Component {
 	render() {
 		return (
-			<Jumbotron fluid>
-				<h1>Hack[in] 2019</h1>
-				<p>Wow this beautiful</p>
-			</Jumbotron>
+			<React.Fragment>
+				<Jumbotron fluid>
+					<h1>Hack[in] 2019</h1>
+					<p>Wow this beautiful</p>
+				</Jumbotron>
+			</React.Fragment>
 		)
 	}
 }
