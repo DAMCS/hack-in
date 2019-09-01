41f88aec49508521d3da0f7c34c4d447c7ca6f29
surya3997 <surya3997@gmail.com>
2019-09-01 22:24:04 +0530

    announcements fixed

--- src/components/Announcement/Announcement.js
+++ src/components/Announcement/Announcement.js
@@ -5,32 +5,34 @@ class Announcement extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			announcement: []
+			announcement: [],
+			seen: 0
 		}
 	}
 	componentDidMount() {
 		var token = localStorage.getItem('token');
-		axios.get('/api/announcement',
-			{
-				headers: {
-					"Authorization": "Bearer " + token
-				}
+		axios.get('/api/announcement', {
+			headers: {
+				"Authorization": "Bearer " + token
 			}
-		).then((res) => {
+		})
+		.then((res) => {
 			this.setState({ 
-				announcement: res.data.data
+				announcement: res.data.data.announcements,
+				seen: res.data.data.seen
 			});
 		})
-			.catch(function (error) {
-				console.log(error);
-			})
+		.catch(function (error) {
+			console.log(error);
+		})
 	}
+
 	render() {
 		return (
 			<div>
 				{
 				this.state.announcement.map((object, i) => {
-					return <div><font color={object.seen === false ? "red" : ""}>{object.annoMsg}</font> <br /></div>
+					return <div><font color={this.state.seen < object.annoId ? "red" : ""}>{object.annoMsg}</font> <br /></div>
 				})}
 			</div>
 		);
