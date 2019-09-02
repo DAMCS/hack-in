df88f8200ec67303dacd1a8497a54ef4bbe64554
Ashfaaq Mohammed <iim.ashfaaq@gmail.com>
2019-09-03 00:15:30 +0530

    LevelOne base added
    Temporary images used. coordinates to be changed later. Inventory to be fixed when in modal. Axios call needs to ba added

--- src/views/LevelOne/LevelOne.js
+++ src/views/LevelOne/LevelOne.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import { Modal, ModalHeader, ModalBody } from 'reactstrap';
 import ReactGA from 'react-ga';
+import Room1 from './Lift3.png';
+import Numpad from './Numpad.js';
 
 function initializeReactGA() {
 	ReactGA.initialize('UA-104887157-5');
@@ -7,12 +10,36 @@ function initializeReactGA() {
 }
 
 export default class LevelOne extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			modal: false
+		};
+	
+		this.toggle = this.toggle.bind(this);
+	}
+	
+	toggle() {
+		this.setState(prevState => ({
+			modal: !prevState.modal
+		}));
+	}
+	
 	render() {
 		initializeReactGA();
 		return (
-			<React.Fragment >
-
-			</React.Fragment>
+			<div className='levelOne'>
+				<img src={Room1} alt='Room One' useMap='#image-map' />
+				<map name="image-map">
+					<area alt="numbed" title="numbed" coords="611,286,656,375" shape="rect" onClick={this.toggle} />
+				</map>
+				<Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+					<ModalHeader toggle={this.toggle}>Enter the Passcode</ModalHeader>
+					<ModalBody>
+						<Numpad />
+					</ModalBody>
+				</Modal>
+			</div>
 		)
 	}
 }
