ad1d768ccd07f1baabf470f67098aca584a94b30
Aakash Hemadri <aakashhemadri123@gmail.com>
2019-09-06 00:07:51 +0530

    Medium obfuscation

---
+++ javascript-obfuscator.js
@@ -0,0 +1,20 @@
+module.exports = {
+	compact: true,
+	controlFlowFlattening: true,
+	controlFlowFlatteningThreshold: 0.75,
+	deadCodeInjection: true,
+	deadCodeInjectionThreshold: 0.4,
+	debugProtection: false,
+	debugProtectionInterval: false,
+	disableConsoleOutput: true,
+	identifierNamesGenerator: 'hexadecimal',
+	log: true,
+	renameGlobals: false,
+	rotateStringArray: true,
+	selfDefending: true,
+	stringArray: true,
+	stringArrayEncoding: 'base64',
+	stringArrayThreshold: 0.75,
+	unicodeEscapeSequence: false
+};
+
